7eeef8c1ea1ced000e87319a5d74bb48bab651aa
bitnara0156 <bitnara0156@gmail.com>
2023-08-22 12:33:37 +0700

    update data

--- src/index.js
+++ src/index.js
@@ -3,35 +3,48 @@ import ReactDOM from "react-dom";
 import DataTable from "react-data-table-component";
 import DataTableExtensions from "react-data-table-component-extensions";
 import "react-data-table-component-extensions/dist/index.css";
-
-import { columns, data } from "./data";
+import { useState, useEffect } from "react";
+import { columns} from "./data";
 
 import "./styles.css";
 
 function App() {
+
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        "https://sheetdb.io/api/v1/rebnctwy2n27y"
+      );
+      const parsed = await response.json();
+      setData(parsed);
+    })();
+  }, []);
   const tableData = {
     columns,
-    data
+    data,
+    setData
   };
-
   return (
     <div className="main">
+
       <DataTableExtensions {...tableData}>
         <DataTable
           columns={columns}
           data={data}
           noHeader
-          defaultSortField="id"
-          defaultSortAsc={false}
+          defaultSortField="ID"
+          defaultSortAsc={true}
 			fixedHeader
 			responsive={true}
 			pagination
-			paginationPerPage={12}
+
 
           highlightOnHover
         />
       </DataTableExtensions>
-	  
+
     </div>
   );
 }
